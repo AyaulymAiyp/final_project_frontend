7ac42825818bc75e89c7fec9a71fb87966593ba7
Aidana <210103080@stu.sdu.edu.kz>
2022-12-20 06:13:57 +0600

    minor changes

---
+++ Pg.js
@@ -0,0 +1,21 @@
+
+// $("document").ready(function(){
+//     $("marquee").mouseover(function() {
+//         $(this).attr('scrollamount',0);
+//         $(this).style.color = "blue";
+//     }).mouseout(function() {
+//          $(this).attr('scrollamount',5);
+//     });
+// });
+
+// window.addEventListener("load", function(e) {
+//    const btn = (e) => {
+//     const $select = document.querySelector('#sel');
+//     $select.value = 'en'
+//   };
+//   document.querySelector('.btn').addEventListener('click', btn); 
+// });
+
+function alertUser(){
+    alert("Unable to change language. Wait till admin is improving her skills");
+}
